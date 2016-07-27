9b8062ac7b4d7f40ba7222b01eff755f1253a83d
Eno Compton <ecompton@pivotal.io>
2016-07-27 11:01:57 +0900

    Add Spring Security session management.
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

---
+++ applications/back-end/src/main/java/io/pivotal/SecurityConfig.java
@@ -0,0 +1,34 @@
+package io.pivotal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/*.js").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .requestCache()
+                .requestCache(new NullRequestCache())
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("fancyuser").password("fancypassword").roles("FANCY_ROLE");
+    }
+}
