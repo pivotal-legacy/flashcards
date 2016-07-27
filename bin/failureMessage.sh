4d3e810044767123d057538b7f54e2241b165af8
Eno Compton <ecompton@pivotal.io>
2016-07-27 12:28:07 +0900

    Change ascii art of failure message.
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- bin/failureMessage.sh
+++ bin/failureMessage.sh
@@ -3,21 +3,18 @@
 RED='\033[1;31m'
 NO_COLOR='\033[0m'
 
+function printFailure {
 echo -e ${RED}
-echo 'FFFFFFFFFFFFFFFFFFFFFF      AAA               IIIIIIIIIILLLLLLLLLLL'
-echo 'F::::::::::::::::::::F     A:::A              I::::::::IL:::::::::L'
-echo 'F::::::::::::::::::::F    A:::::A             I::::::::IL:::::::::L'
-echo 'FF::::::FFFFFFFFF::::F   A:::::::A            II::::::IILL:::::::LL'
-echo '  F:::::F       FFFFFF  A:::::::::A             I::::I    L:::::L'
-echo '  F:::::F              A:::::A:::::A            I::::I    L:::::L'
-echo '  F::::::FFFFFFFFFF   A:::::A A:::::A           I::::I    L:::::L'
-echo '  F:::::::::::::::F  A:::::A   A:::::A          I::::I    L:::::L'
-echo '  F:::::::::::::::F A:::::A     A:::::A         I::::I    L:::::L'
-echo '  F::::::FFFFFFFFFFA:::::AAAAAAAAA:::::A        I::::I    L:::::L'
-echo '  F:::::F         A:::::::::::::::::::::A       I::::I    L:::::L'
-echo '  F:::::F        A:::::AAAAAAAAAAAAA:::::A      I::::I    L:::::L         LLLLLL'
-echo 'FF:::::::FF     A:::::A             A:::::A   II::::::IILL:::::::LLLLLLLLL:::::L'
-echo 'F::::::::FF    A:::::A               A:::::A  I::::::::IL::::::::::::::::::::::L'
-echo 'F::::::::FF   A:::::A                 A:::::A I::::::::IL::::::::::::::::::::::L'
-echo 'FFFFFFFFFFF  AAAAAAA                   AAAAAAAIIIIIIIIIILLLLLLLLLLLLLLLLLLLLLLLL'
+echo '  █████▒▄▄▄       ██▓ ██▓         █████▒▄▄▄       ██▓ ██▓         █████▒▄▄▄       ██▓ ██▓'
+echo '▓██   ▒▒████▄    ▓██▒▓██▒       ▓██   ▒▒████▄    ▓██▒▓██▒       ▓██   ▒▒████▄    ▓██▒▓██▒'
+echo '▒████ ░▒██  ▀█▄  ▒██▒▒██░       ▒████ ░▒██  ▀█▄  ▒██▒▒██░       ▒████ ░▒██  ▀█▄  ▒██▒▒██░'
+echo '░▓█▒  ░░██▄▄▄▄██ ░██░▒██░       ░▓█▒  ░░██▄▄▄▄██ ░██░▒██░       ░▓█▒  ░░██▄▄▄▄██ ░██░▒██░'
+echo '░▒█░    ▓█   ▓██▒░██░░██████▒   ░▒█░    ▓█   ▓██▒░██░░██████▒   ░▒█░    ▓█   ▓██▒░██░░██████▒'
+echo ' ▒ ░    ▒▒   ▓▒█░░▓  ░ ▒░▓  ░    ▒ ░    ▒▒   ▓▒█░░▓  ░ ▒░▓  ░    ▒ ░    ▒▒   ▓▒█░░▓  ░ ▒░▓  ░'
+echo ' ░       ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░    ░       ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░    ░       ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░'
+echo ' ░ ░     ░   ▒    ▒ ░  ░ ░       ░ ░     ░   ▒    ▒ ░  ░ ░       ░ ░     ░   ▒    ▒ ░  ░ ░'
+echo '             ░  ░ ░      ░  ░                ░  ░ ░      ░  ░                ░  ░ ░      ░  ░'
 echo -e ${NO_COLOR}
+}
+
+printFailure
